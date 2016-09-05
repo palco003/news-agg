72854561ce0bdf5021262c08da7b27932c6a9dc1
Pablo Alcocer <palco003@fiu.edu>
2016-09-05 00:41:02 -0400

    added form and base template

--- news/views.py
+++ news/views.py
@@ -1,6 +1,11 @@
+from django.shortcuts import redirect
 from django.shortcuts import render
-from .models import Article, Feed
+from django.urls import reverse
+
 from .forms import FeedForm
+from .models import Article, Feed
+import feedparser
+import datetime
 
 # Create your views here.
 
@@ -16,5 +21,31 @@ def feeds_list(request):
 
 
 def new_feed(request):
-    form = FeedForm()
+    if request.method == "POST":
+        form = FeedForm(request.POST)
+        if form.is_valid():
+            feed = form.save(commit=False)
+
+            feedData = feedparser.parse(feed.url)
+
+            # set some fields
+            feed.title = feedData.feed.title
+            feed.save()
+
+            for entry in feedData.entries:
+                article = Article()
+                article.title = entry.title
+                article.url = entry.link
+                article.description = entry.description
+                # publication_date
+                d = datetime.datetime(*(entry.published_parsed[:6]))
+                date_string = d.strftime('%Y-%m-%d %H:%M:%S')
+
+                article.publication_date = date_string
+                article.feed = feed
+                article.save()
+
+            return redirect(reverse('feeds_list'))
+    else:
+        form = FeedForm()
     return render(request, 'news/new_feed.html', {'form': form})
