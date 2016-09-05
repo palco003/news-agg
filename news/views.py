fe2e9df98ff2361237c7ac70e307eb04f48d52c7
Pablo Alcocer <palco003@fiu.edu>
2016-09-04 23:19:34 -0400

    added form and base template

--- news/views.py
+++ news/views.py
@@ -1,7 +1,20 @@
 from django.shortcuts import render
+from .models import Article, Feed
+from .forms import FeedForm
 
 # Create your views here.
 
 
 def articles_list(request):
-    return render(request, 'news/articles_list.html', {})
+    articles = Article.objects.all()
+    return render(request, 'news/articles_list.html', {'articles': articles})
+
+
+def feeds_list(request):
+    feeds = Feed.objects.all()
+    return render(request, 'news/feeds_list.html', {'feeds': feeds})
+
+
+def new_feed(request):
+    form = FeedForm()
+    return render(request, 'news/new_feed.html', {'form': form})
